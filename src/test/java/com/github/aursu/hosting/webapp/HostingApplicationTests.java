8c0bd4348f352dc6fc7f313fdc71cc51d095ae4c
Alexander Ursu <alexander.ursu@gmail.com>
2022-12-30 16:27:30 +0100

    first commit

---
+++ src/test/java/com/github/aursu/hosting/webapp/HostingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.aursu.hosting.webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HostingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
