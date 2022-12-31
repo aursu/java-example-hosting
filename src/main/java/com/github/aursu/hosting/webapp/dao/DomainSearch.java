6aac76593a53b9f53ab9d7d8220816536fe820a9
Alexander Ursu <alexander.ursu@gmail.com>
2022-12-31 04:43:28 +0100

    added DAO object to save  domain search data

--- src/main/java/com/github/aursu/hosting/webapp/dao/DomainSearch.java
+++ src/main/java/com/github/aursu/hosting/webapp/dao/DomainSearch.java
@@ -1,2 +1,15 @@
-package com.github.aursu.hosting.webapp.dao;public class DomainSearch {
+package com.github.aursu.hosting.webapp.dao;
+
+import com.github.aursu.hosting.webapp.entities.Domain;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class DomainSearch {
+    private String domainName;
+    private Domain domain;
+
+    private String webPage;
+    private String action;
 }
