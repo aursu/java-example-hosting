02c419a98c434a15b3f606e66574d8ad1ef1307e
Alexander Ursu <alexander.ursu@gmail.com>
2022-12-31 04:44:18 +0100

    Added domain validation

--- src/main/java/com/github/aursu/hosting/webapp/services/DomainService.java
+++ src/main/java/com/github/aursu/hosting/webapp/services/DomainService.java
@@ -1,2 +1,12 @@
-package com.github.aursu.hosting.webapp.services;public class DomainService {
+package com.github.aursu.hosting.webapp.services;
+
+import org.springframework.stereotype.Component;
+
+import com.google.common.net.InternetDomainName;
+
+@Component
+public class DomainService {
+    public boolean isValidDomain(String domain) {
+        return InternetDomainName.isValid(domain);
+    }
 }
