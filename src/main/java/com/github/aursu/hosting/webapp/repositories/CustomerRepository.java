c9e53c1e30b0ce6b8a641b7192e7cf0ed39eea75
Alexander Ursu <alexander.ursu@gmail.com>
2023-01-03 15:03:31 +0100

    Removed unused import

--- src/main/java/com/github/aursu/hosting/webapp/repositories/CustomerRepository.java
+++ src/main/java/com/github/aursu/hosting/webapp/repositories/CustomerRepository.java
@@ -2,7 +2,6 @@ package com.github.aursu.hosting.webapp.repositories;
 
 import com.github.aursu.hosting.webapp.entities.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
