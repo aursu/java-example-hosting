e4f15f05751da8280ba0c7bcf67dcf350145de71
Alexander Ursu <alexander.ursu@gmail.com>
2023-01-04 16:40:01 +0100

    Added method to update password

--- src/main/java/com/github/aursu/hosting/webapp/services/DomainService.java
+++ src/main/java/com/github/aursu/hosting/webapp/services/DomainService.java
@@ -1,12 +1,31 @@
 package com.github.aursu.hosting.webapp.services;
 
-import org.springframework.stereotype.Component;
-
+import com.github.aursu.hosting.webapp.entities.Domain;
+import com.github.aursu.hosting.webapp.repositories.DomainRepository;
 import com.google.common.net.InternetDomainName;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
-@Component
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
 public class DomainService {
+    private final DomainRepository domainRepository;
+
     public boolean isValidDomain(String domain) {
         return InternetDomainName.isValid(domain);
+    }
+
+    public void updatePassword(Domain domain, String password1, String password2) {
+        String domainName = domain.getId();
+
+        Optional<Domain> domainLookup = domainRepository.findById(domainName);
+        if (domainLookup.isPresent()  && password2.equals(password1)) {
+            Domain domainEntity = domainLookup.get();
+            domainEntity.setPassword(password1);
+
+            domainRepository.save(domainEntity);
+        }
     }
 }
